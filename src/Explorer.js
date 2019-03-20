aced0778a634ce2be476bc86664b3e7655c27a94
vsabbani <venugopal.sabbani@move.com>
2019-03-20 10:07:20 +0530

    Explore view is done

--- src/Explorer.js
+++ src/Explorer.js
@@ -1,11 +1,44 @@
 import React, { Component } from "react";
+import { Card, Button, CardImg, CardTitle, CardText, CardDeck,
+    CardSubtitle, CardBody } from 'reactstrap';
 import MovieCard from './movieCard';
+import listData from "./apiData/exploreView.json"
+import './App.css';
+
 
 
 class Explorer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+   
     render() {
+        let rowList, rowLength;
       return(
-        <MovieCard/>
+        <React.Fragment>
+        {listData["Search"].map((movie,index)=>{
+             rowList = [listData["Search"][index], listData["Search"][index+1], listData["Search"][index+2], listData["Search"][index+3]]
+             if(index%4 == 0){
+                rowLength = 0; 
+                return(
+                <div className="row listClass"  key={index+1}>
+                <CardDeck key={index+1}>
+                {rowList.map((movieData,rowindex)=> {
+                  rowLength += 1
+                   if(movieData){
+                       return(<MovieCard movieData={movieData} key={rowindex+1} rowLength={rowLength}/>)
+                   }
+                })};
+                </CardDeck>
+              </div>) 
+             }
+             else{
+               return null
+             } 
+        })};
+        </React.Fragment>
       )
     }
 }
--- src/movieCard.js
+++ src/movieCard.js
@@ -4,57 +4,24 @@ import { Card, Button, CardImg, CardTitle, CardText, CardDeck,
 import './App.css';
 import listData from "./apiData/exploreView.json"
 
-console.log(11111)
-console.log(listData["Search"])
-
-
 const  MovieCard = (props) => {
     const cardWidth = {
         width: '18rem' 
     }
-    listData["Search"].map((value,index)=>{
-      console.log(value)
-    })
+    let movieList;
   return(
-    // <React.Fragment>
-    //     <div className="row">
-    //     <div className="col-md-4">
-    //         <div className="thumbnail">
-    //         <a href="https://m.media-amazon.com/images/M/MV5BZjA0OWVhOTAtYWQxNi00YzNhLWI4ZjYtNjFjZTEyYjJlNDVlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg">
-    //             <img src="https://m.media-amazon.com/images/M/MV5BZjA0OWVhOTAtYWQxNi00YzNhLWI4ZjYtNjFjZTEyYjJlNDVlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="Lights" style={cardWidth}/>
-    //             <div className="caption">
-    //             <p>.</p>
-    //             </div>
-    //         </a>
-    //         </div>
-    //     </div> 
-    // </div>
-    // <div className="card">
-    //     <img className="card-img-top" src="https://m.media-amazon.com/images/M/MV5BZjA0OWVhOTAtYWQxNi00YzNhLWI4ZjYtNjFjZTEyYjJlNDVlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="Card image cap" style={cardWidth}/>
-    //     <div className="card-body">
-    //         <h5 className="card-title">Card title</h5>
-    //         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    //         <a href="#" className="btn btn-primary">Go somewhere</a>
-    //     </div> 
-    // </div>  
-    // </React.Fragment>
-    
-    <div className="row">
-    <CardDeck>
-    <div className="col-md-3">
-      <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-    </div>
-    </CardDeck>
-    </div>
-  ) 
+        <div className="">
+          <Card>
+            <CardImg top width="100%" src={props.movieData.Poster} alt="Card image cap" />
+            <CardBody>
+              <CardTitle>{props.movieData.Title}</CardTitle>
+              <CardSubtitle>Year: {props.movieData.Year}</CardSubtitle>
+              <CardText>Type: {props.movieData.Type}</CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </div>
+  )
 } 
 
 export default MovieCard;
